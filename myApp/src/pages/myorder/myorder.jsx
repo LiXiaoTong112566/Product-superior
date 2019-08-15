9029fcd5982461e9be86598ba274ffdc00b0fe65
dzh <1251763784@qq.com>
2019-08-15 09:43:21 +0800

    8.15

--- myApp/src/pages/myorder/myorder.jsx
+++ myApp/src/pages/myorder/myorder.jsx
@@ -2,8 +2,8 @@
 
 import Taro, { Component } from '@tarojs/taro'
 import { View, Image } from '@tarojs/components'
-import { observer, inject } from '@tarojs/mobx'
-import wudingdan from '../../static/images/wudingdan.png'
+// import { observer, inject } from '@tarojs/mobx'
+// import wudingdan from '../../static/images/wudingdan.png'
 import bigimg from '../../img/baobao.jpg'
 
 import './myorder.scss'
@@ -50,18 +50,17 @@ class Myorder extends Component {
           <View className='conitem'>
             <View className='conitemtop'>
               <View className='time'>2019-01-17 17:18:50</View>
-              <View className='status'>代发货</View>
+              <View className='status'>待发货</View>
             </View>
             <View className='info'>
               <Image src={bigimg} className='img'></Image>
-              <View >
+              <View className='infocon'>
                 <View>帮宝适绿帮纸尿裤大号的看法内开内倒不能地方不能</View>
                 <View>规格：XL</View>
                 <View>
                   <View>￥52.50</View>
                   <View>x1</View>
                 </View>
-
               </View>
               <View>共2件商品 合计：￥52.50</View>
             </View>
